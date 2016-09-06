74c46cceef01591a2a892c0325cd9774496be62e
Syed Rubab Redwan <rubabredwan@gmail.com>
2016-09-06 23:05:57 +0600

    added template file

---
+++ template.cpp
@@ -0,0 +1,26 @@
+/* Bismillahir Rahmanir Rahim */
+
+#include <bits/stdc++.h>
+
+#define read(x)		freopen(x, "r", stdin)
+#define write(x) 	freopen(x, "w", stdout)
+
+#define rep(i, n)	for(int i=0;i<n;i++)
+#define repn(i, n)	for(int i=1;i<=n;i++)
+#define set(i, n)	memset(i, n, sizeof(i))
+
+#define mx			10005
+#define pb			push_back
+
+#define inf			(1 << 29)	
+#define	eps			1e-6
+#define pi 			acos(-1)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main(){
+	return 0;
+}
